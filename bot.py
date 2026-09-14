9adf12e4216054bd5768bc7cbf5784ff795e36a1
dinuparmar909-debug <dinuparmar909@gmail.com>
2026-09-14 18:45:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
 import os, time, yfinance as yf, requests
 from datetime import datetime
+from flask import Flask
+import threading
+
+# --- Render ke liye fake website ---
+app = Flask('')
+@app.route('/')
+def home(): return "BOT LIVE - Nifty 500 Scan Running!"
+def run_flask():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+threading.Thread(target=run_flask).start()
+# -----------------------------------
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
@@ -23,4 +34,5 @@ while True:
             send_telegram(f"🔥 RELIANCE {round(change,2)}% - BUY Signal")
     except:
         pass
+
     time.sleep(900) # 15 min
